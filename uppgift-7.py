3b53414ef285df1a388cd78e0538ef1608748abe
tcpwalker <55358263+tcp-walker@users.noreply.github.com>
2022-11-12 16:52:21 +0100

    Add files via upload

---
+++ uppgift-7.py
@@ -0,0 +1,12 @@
+n=0
+lon = 0
+sum_lon = 0
+antal = int(input(" Antal dagar = "))
+while n <= antal-1 :
+   lon = 2**n
+   sum_lon+= lon
+   #print (n, lon, sum_lon)
+   if (sum_lon >= 100000000):
+        print(" en miljon", n, " dagar" ) 
+   n+=1 
+print (" summa pengar = ", sum_lon) 
